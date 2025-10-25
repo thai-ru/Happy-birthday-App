3bbec0fbc6bba4c11b4fe81a6c17dac4a4c1f441
thai-ru <robinthairu34@gmail.com>
2025-10-25 07:24:59 +0300

    update: add image @Composable

--- app/src/main/java/com/thairu/birthdayapp/MainActivity.kt
+++ app/src/main/java/com/thairu/birthdayapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,11 +15,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.thairu.birthdayapp.ui.theme.BirthdayAppTheme
+import androidx.compose.foundation.Image
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -64,6 +67,15 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Image(
+        painter = image,
+        contentDescription = null
+    )
+}
+
 @Preview(
     showBackground = true,
     showSystemUi = true,
@@ -73,6 +85,12 @@ fun BirthdayCardPreview() {
     BirthdayAppTheme {
         GreetingText(
             message = "Happy Birthday Sam!",
-            from = "From Josh")
+            from = "From Josh"
+        )
+
+        GreetingImage(
+            message = "Happy Birthday Liz!",
+            from = "Robinson"
+        )
     }
 }
